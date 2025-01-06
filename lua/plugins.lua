d2926e75f3ab047c387d4ff28c032eb337164e3c
Dwi Asmoro Bangun <dwiaceromo@gmail.com>
2025-01-06 19:38:45 +0700

    add multicursors.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -355,6 +355,24 @@ local plugins      = {
         lazy = false,
     },
 
+    {
+        "smoka7/multicursors.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "nvimtools/hydra.nvim",
+        },
+        opts = {},
+        cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+        keys = {
+            {
+                mode = { "v", "n" },
+                "<Leader>md",
+                "<cmd>MCstart<cr>",
+                desc = "Create a selection for selected text or word under the cursor",
+            },
+        },
+    },
+
     -- UI / Looks
     {
         "Bekaboo/dropbar.nvim",
